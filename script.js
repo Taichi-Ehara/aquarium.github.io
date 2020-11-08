fba71c7ba5de3a2e0d0f22785cd6b9f895eee3a7
Taichi-Ehara <72008602+Taichi-Ehara@users.noreply.github.com>
2020-11-09 00:02:33 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function() {
+  $('#menuButton').click(function() {
+    if ($('.icon').hasClass('close')) {
+      $('.icon').removeClass('close');
+
+      $('.hide-menu-wrapper').animate({
+
+        'margin-right': '-300px'
+
+      }, 200, 'swing');
+
+    } else {
+      $('.icon').addClass('close');
+
+      $('.hide-menu-wrapper').animate({
+
+        'margin-right': '0'
+
+      }, 200, 'swing');
+    }
+  });
+
+  $('header a').click(function() {
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+
+    $('html, body').animate({
+      'scrollTop' : position
+    }, 500);
+  });
+});
